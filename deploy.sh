e66b9a6710d54213313460508cdd0249390b7860
Lee Boonstra <me@leeboonstra.com>
2021-04-29 12:03:01 +0200

    envsubst

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,8 @@ gcloud builds submit --config _cloudbuilder/setup.yaml
 
 bold "Eval the templates & deploy the containers..."
 # https://stackoverflow.com/questions/23620827/envsubst-command-not-found-on-mac-os-x-10-8
-envsubst < _cloudbuilder/chatserver-deployment.yaml | kubectl apply -f -
-envsubst < _cloudbuilder/web-deployment.yaml | kubectl apply -f -
+cat _cloudbuilder/chatserver-deployment.yaml | envsubst | kubectl apply -f -
+cat _cloudbuilder/web-deployment.yaml | envsubst | kubectl apply -f -
 
 bold "Create services..."
 kubectl apply -f _cloudbuilder/services.yaml
