6726b07e14c6f8e531d6a093b5919f773a108a98
Lee Boonstra <me@leeboonstra.com>
2021-05-05 17:53:46 +0200

    fix load vars script

--- load-env.sh
+++ load-env.sh
@@ -1,3 +1,4 @@
+kubectl delete configmap chatserver-config
 kubectl create configmap chatserver-config --from-env-file=backend/.env
 kubectl scale deployment chat --replicas=0 -n service
 kubectl scale deployment chat --replicas=1 -n service
