7339db39b2c746f8c320c87fd9f102033cdaaeaa
Lee Boonstra <me@leeboonstra.com>
2021-05-05 18:01:21 +0200

    fix deployment names

--- load-env.sh
+++ load-env.sh
@@ -1,4 +1,4 @@
 kubectl delete configmap chatserver-config
 kubectl create configmap chatserver-config --from-env-file=backend/.env
-kubectl scale deployment chat --replicas=0 -n chatserver
-kubectl scale deployment chat --replicas=1 -n chatserver
+kubectl scale deployment chatserver --replicas=0
+kubectl scale deployment chatserver --replicas=1
