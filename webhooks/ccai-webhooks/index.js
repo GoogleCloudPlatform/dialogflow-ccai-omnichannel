9b1f5c8dbbf4f3df474f616f5622ded5a0ef4df1
Lee Boonstra <lee@leeboonstra.com>
2021-05-14 14:00:55 +0200

    working webhooks

--- webhooks/ccai-webhooks/index.js
+++ webhooks/ccai-webhooks/index.js
@@ -5,25 +5,29 @@ const axios = require('axios');
 async function textMsg() {
     const path = '/api/sms/confirmation/';
 
-    let results = await doRequest(data, path);
+    let results = await doRequest(path);
     return results;
 }
 
 async function call() {
     const path = '/api/callme/';
 
-    let results = await doRequest(data, path);
+    let results = await doRequest(path);
     return results;
 }
 
-async function doRequest(data, path){
-    axios.get(`https://www.conv.dev${path}`)
-      .then(function (results) {
-        console.log(results);
-      })
-      .catch(function (error) {
-        console.log(error);
-    });
+async function doRequest(path){
+    var resp;
+    try {
+        resp = await axios.get('https://www.conv.dev' + path);
+        console.log(resp);
+    } catch (err) {
+        // Handle Error Here
+        console.error(err);
+        resp = err;
+    }
+
+    return resp;
 }
 
 async function handleRequest(map, request){
@@ -31,21 +35,26 @@ async function handleRequest(map, request){
     if(request.body && request.body.queryResult && request.body.queryResult.intent){
       intent = request.body.queryResult.intent.displayName;
     }
+    console.log('---');
+    console.log(intent);
     let results;	
-    if (map.has(intent) !== false){
-        results = await map.get(intent)(request);
+    if (map.has(intent)){
+        results = await map.get(intent)();
         console.log(results);
     } else {
-      results = map.get('Default Fallback Intent')(request);
+      results = {
+          success: false
+      };
     }
-    response.json(results);
+    return results;
 }
 
-exports.sendConfirmation = function sendConfirmation (request, response) {
+exports.sendConfirmation = async function sendConfirmation(request, response) {
     let intentMap = new Map();
     intentMap.set('callme-now', call);
     intentMap.set('confirm-appointment', textMsg);
-    let webhookResponse = handleRequest(intentMap, request, response);
+
+    let webhookResponse = await handleRequest(intentMap, request);
     console.log(webhookResponse);
-    response.json(webhookResponse)
+    response.json(webhookResponse);
 };
